240579923c48f775ba69221784d1ae90876586ec
Luca Caorsi <lcaorsi2702@gmail.com>
2025-06-29 18:44:05 +0000

    Agrego script de build para Render con Node 18

--- .render/project-build.sh
+++ .render/project-build.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+echo "Usando Node 18 para compilación"
+
+nvm install 18
+nvm use 18
+
+bundle install
+bundle binstubs bundler --force
+yarn install
+yarn build#!/bin/bash
 echo "Instalando dependencias..."
 corepack enable
 corepack prepare yarn@1.22.22 --activate
